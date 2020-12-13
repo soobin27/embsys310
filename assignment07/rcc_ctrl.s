2468ab8f3582ba8206329b2d5b38573e563ba215
Scott Chun <soobin27@hotmail.com>
2020-12-12 21:59:31 -0800

    Add files via upload

---
+++ assignment07/rcc_ctrl.s
@@ -0,0 +1,61 @@
+/*******************************************************************************
+File name       : rcc_ctrl.s
+Description     : Assembly language function for controlling the user LED
+*******************************************************************************/   
+
+    PUBLIC enable_rcc        // Exports symbols to other modules
+
+// Code is split into logical sections using the SECTION directive.
+// Source: http://ftp.iar.se/WWWfiles/arm/webic/doc/EWARM_AssemblerReference.ENU.pdf
+// SECTION  section  :type [:flag] [(align)]
+//      The data section is used for declaring initialized data or constants. This data does not change at runtime
+//      The bss section is used for declaring variables. The syntax for declaring bss section is -
+//      The text section is used for keeping the actual code.
+
+// CODE: Interprets subsequent instructions as Arm or Thumb instructions, 
+// depending on the setting of related assembler options.
+
+// NOREORDER (the default mode) starts a new fragment in the section
+// with the given name, or a new section if no such section exists.
+// REORDER starts a new section with the given name.
+
+// NOROOT means that the section fragment is discarded by the linker if
+// no symbols in this section fragment are referred to. Normally, all
+// section fragments except startup code and interrupt vectors should
+// set this flag.
+
+// The (2) is for the (align)
+// The power of two to which the address should be aligned.
+// The permitted range is 0 to 8. 
+// Code aligned at 4 Bytes.
+
+    SECTION .text:CODE:REORDER:NOROOT(2)
+    
+    THUMB               // Indicates THUMB code is used
+                        // Subsequent instructions are assembled as THUMB instructions
+    
+/*******************************************************************************
+Function Name   : enable_rcc
+Description     : - Uses Bit-Band registers at base 0x4200.0000
+                    to set GPIOA Output Data Register.
+                  - Calls another assembly function delay to insert
+                    delay loop
+
+C Prototype     : void enable_rcc(uint32_t port)
+                : Where port indicates wich port to enable the clock for
+Parameters      : R0: uint32_t port
+
+Return value    : None
+*******************************************************************************/  
+
+// Bitband address calculation formula
+// (0x42000000+(0x2104C *32) + (1*4))) = 0x1;
+
+enable_rcc
+    // <TODO> Implement function in assembly
+    MOVW     R0, #0x0980    // load the LSW
+    MOVT     R0, #0x4242    // load the MSW
+    MOVW     R1, #1         // load 1 to R1 to set bit 1 of bitband RCC
+    STR      R1, [R0]       // store 1 to address of bitband RCC
+    BX       LR             // return to main
+    END
