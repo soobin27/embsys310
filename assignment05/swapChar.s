99c3e38e3ed422d672d797ee2028ace5bff8fc9e
Scott Chun <soobin27@hotmail.com>
2020-12-12 21:26:02 -0800

    Update swapChar.s

--- assignment05/swapChar.s
+++ assignment05/swapChar.s
@@ -18,16 +18,16 @@ Return value    : R0
 // For some reason in the Tera Term output, PrintString function printed extra letter. Did not have time to debug...
 
 swapChar
-    CMP	        R0, R1          // compare value of R0 and R1
-    BNE 	swap            // branch if not equal, skip to label swap
-    MOVS        R0, #0          // else preparing to return 0 by moving 0 to R0
-    B          exit             // branch to exit back to LR location
+    CMP	        R0, R1      // compare value of R0 and R1
+    BNE 	    swap        // branch if not equal, skip to label swap
+    MOVS        R0, #0      // *** BONUS return 0 two values are identical ***
+    B          exit         // branch to exit back to LR location
 swap
     LDRB	R2, [R0]        // move value stored in address of R0 to R2
     LDRB	R3, [R1]        // move value stored in address of R1 to R3
-    STRB        R3, [R0]        // store value in R3 to memory address in R0
-    STRB        R2, [R1]        // store value in R2 to memory address in R0
-    MOVS	R0, #1
-exit                            // exit label   
+    STRB    R3, [R0]        // store value in R3 to memory address in R0
+    STRB    R2, [R1]        // store value in R2 to memory address in R0
+    MOVS	R0, #1          // *** BONUS return 1 if not identical ***
+exit                       // exit label   
     BX  LR
     END
